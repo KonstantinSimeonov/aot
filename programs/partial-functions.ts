a6b351d248023b107ab6c065fdf103af93eb4e0f
Konstantin Simeonov <kon.simeonov@protonmail.com>
2022-07-18 19:42:03 +0300

    more elegant Partialize

--- programs/partial-functions.ts
+++ programs/partial-functions.ts
@@ -1,45 +1,61 @@
-type AnyFunction = (...args: any[]) => any
+type AnyFunction = (...args: any[]) => any;
 
-type PartialArgs<T extends any[], Acc extends any[] = []> =
-    T extends [infer X, ...infer XS]
-        ? (Acc | PartialArgs<XS, [...Acc, X]>)
-        : Acc
+type PartialArgs<T extends any[], Acc extends any[] = []> = T extends [
+  infer X,
+  ...infer XS
+]
+  ? Acc | PartialArgs<XS, [...Acc, X]>
+  : Acc;
 
-type Drop<A extends any[], B extends any[]> =
-  A extends [infer X, ...infer XS]
-    ? B extends [infer Y, ...infer YS]
-      ? Y extends X ? Drop<XS, YS> : A
+type Drop<A extends any[], B extends any[]> = A extends [infer X, ...infer XS]
+  ? B extends [infer Y, ...infer YS]
+    ? Y extends X
+      ? Drop<XS, YS>
       : A
     : A
+  : A;
 
-type PFN<FN extends AnyFunction, Acc extends any[]> = {
-  (): PFN<FN, Acc>
-  <A extends PartialArgs<Drop<Parameters<FN>, Acc>>>(...args: A): [...Acc, ...A] extends Parameters<FN>
-    ? ReturnType<FN>
-    : PFN<FN, [...Acc, ...A]>
-}
+type PartializeAccum<Fn extends AnyFunction, Acc extends any[]> = {
+  (): PartializeAccum<Fn, Acc>;
+  <A extends PartialArgs<Drop<Parameters<Fn>, Acc>>>(...args: A): [
+    ...Acc,
+    ...A
+  ] extends Parameters<Fn>
+    ? ReturnType<Fn>
+    : PartializeAccum<Fn, [...Acc, ...A]>;
+};
 
-const partialize = <FN extends AnyFunction>(fn: FN): PFN<FN, []> =>
-  function partial(...args) {
-    if (args.length === 0) return partial
-    if (args.length >= fn.length) return fn(...args)
-    return (...next_args) => partial(...args, ...next_args)
-  }
+type Partialize<Fn extends AnyFunction> = {
+  (): Partialize<Fn>;
+  <Args extends PartialArgs<Parameters<Fn>>>(
+    ...args: Args
+  ): Args extends Parameters<Fn>
+    ? ReturnType<Fn>
+    : Partialize<(...args: Drop<Parameters<Fn>, Args>) => ReturnType<Fn>>;
+};
 
-const add4 = (a: number, b: string, c: boolean, d: number): string => a + b + c + d
-const p4 = partialize(add4)
+const partialize = <FN extends AnyFunction>(fn: FN): Partialize<FN> =>
+  function partial(...args: readonly unknown[]) {
+    if (args.length === 0) return partial;
+    if (args.length >= fn.length) return fn(...args);
+    return (...next_args: readonly unknown[]) => partial(...args, ...next_args);
+  };
+
+const add4 = (a: number, b: string, c: boolean, d: number): string =>
+  a + b + c + d;
+const p4 = partialize(add4);
 
 console.log(
-  p4(1, 'a')(true)(4),
-  p4(1)('a')(true)(4),
-  p4()()()()(1)()('a')()()(true, 4),
-  p4(1, 'a', true, 4),
+  p4(1, "a")(true)(4),
+  p4(1)("a")(true)(4),
+  p4()()()()(1)()("a")()()(true, 4),
+  p4(1, "a", true, 4),
   // @ts-expect-error
-  p4(1)('a', true)('4'),
+  p4(1)("a", true)("4"),
   // @ts-expect-error
-  p4({}, 'a', true, 4),
-  [1, 2, 3, 4].map(p4(1)('a', true)),
+  p4({}, "a", true, 4),
+  [1, 2, 3, 4].map(p4(1)("a", true)),
 
   // buggy, should be error
-  [1, 2, 3, 4, '4'].map(p4(1)('a', true)),
-)
+  [1, 2, 3, 4, "4"].map(p4(1)("a", true))
+);
