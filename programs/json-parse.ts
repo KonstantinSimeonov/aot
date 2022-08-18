1bd3bd89bef08d1469ec3c3a4a0bda7c3dc0965b
Konstantin Simeonov <kon.simeonov@protonmail.com>
2022-08-18 12:12:19 +0300

    use fail-success

--- programs/json-parse.ts
+++ programs/json-parse.ts
@@ -1,3 +1,4 @@
+export {}
 type Digits = `0` | `1` | `2` | `3` | `4` | `5` | `6` | `7` | `8` | `9`;
 
 type Str2Dec<
@@ -5,30 +6,33 @@ type Str2Dec<
   Acc extends any[] = []
 > = `${T}` extends `${Acc[`length`]}` ? Acc[`length`] : Str2Dec<T, [...Acc, 0]>;
 
-type Fail<T extends string, Context = ``> = { __reason: T; __context: Context };
+type Fail<Reason extends string, Ctx> = { __reason: Reason; ctx: Ctx }
+type Success<V, Rest> = { __value: V; rest: Rest }
 
-type ParseInt<T, N extends string = ``> = T extends `${infer X}${infer XS}`
-  ? X extends Digits
-    ? ParseInt<XS, `${N}${X}`>
-    : [N extends `` ? Fail<`not a number`, T> : Str2Dec<N>, T]
-  : [N extends `` ? Fail<`not a number`, T> : Str2Dec<N>, T];
+type ParseInt<
+  T,
+  _n extends string = ``,
+  _result = _n extends `` ? Fail<`not a number`, T> : Success<Str2Dec<_n>, T>
+> = T extends `${infer D}${infer XS}`
+  ? D extends Digits
+    ? ParseInt<XS, `${_n}${D}`>
+    : _result
+  : _result;
 
 type ParseBoolean<T> = T extends `true${infer XS}`
-  ? [true, XS]
+  ? Success<true, XS>
   : T extends `false${infer YS}`
-  ? [false, YS]
-  : [Fail<`not a bool`, T>, T];
+  ? Success<false, YS>
+  : Fail<`not a bool`, T>
 
 type ParseString<T> = T extends `"${infer Str}"${infer Rest}`
-  ? [`"${Str}"`, Rest]
-  : [Fail<`not a string`, T>, T];
+  ? Success<Str, Rest>
+  : Fail<`not a string`, T>
 
-type OneOf<T> = T extends [infer PR, ...infer PRS]
-  ? PR extends [infer X, infer XS]
-    ? X extends Fail<any, any>
-      ? OneOf<PRS>
-      : [X, XS]
-    : OneOf<PRS>
+type OneOf<T> = T extends [infer ParseResult, ...infer Rest]
+  ? ParseResult extends Success<any, any>
+      ? ParseResult
+      : OneOf<Rest>
   : Fail<`no parser matched`, T>;
 
 type Comma<T> = T extends `,${infer XS}` ? XS : T;
@@ -41,64 +45,56 @@ type ParseValue<T> = OneOf<
 >;
 
 type ParseArrayItems<T, Result extends any[] = []> = T extends `]${infer XS}`
-  ? [Result, XS]
-  : ParseValue<T> extends [infer V, infer Rest]
-  ? V extends Fail<infer Reason, infer Ctx>
-    ? [Fail<`could not parse array value: ${Reason}`, Ctx>, T]
-    : ParseArrayItems<Whitespace<Comma<Rest>>, [...Result, V]>
-  : [Fail<`unexpected token`, T>, T];
+  ? Success<Result, XS>
+  : ParseValue<T> extends Success<infer V, infer Rest>
+    ? ParseArrayItems<Whitespace<Comma<Rest>>, [...Result, V]>
+    : Fail<`could not parse array value`, T>
 
 type ParseArray<T> = T extends `[${infer XS}`
   ? ParseArrayItems<XS>
-  : [Fail<`expected [`, T>, T];
+  : Fail<`expected [`, T>
 
-type ParseKVP<T> = ParseString<T> extends [infer Key, infer Rest]
-  ? Key extends `"${infer Key}"`
-    ? Whitespace<Rest> extends `:${infer Rest2}`
-      ? ParseValue<Whitespace<Rest2>> extends [infer V, infer Rest3]
-        ? V extends Fail<infer Reason, infer Ctx>
-          ? [Fail<`could not parse kvp value: ${Reason}`, Ctx>, T]
-          : [{ [k in Key]: V }, Rest3]
-        : [Fail<`parser returned stupid stuff`, T>, T]
-      : [Fail<`expected :`, T>, T]
-    : [Fail<`string parser returned stupid stuff`, T>, T]
-  : [Fail<`whatevs`>, T];
+type ParseKVP<T> = ParseString<T> extends Success<`${infer Key}`, infer Rest>
+  ? Whitespace<Rest> extends `:${infer Rest2}`
+    ? ParseValue<Whitespace<Rest2>> extends Success<infer V, infer Rest3>
+        ? Success<{ [k in Key]: V }, Rest3>
+        : Fail<`could not parse kvp value`, T>
+    : Fail<`expected :`, T>
+  : Fail<`string parser returned stupid stuff`, T>
 
 type ParseObjectEntries<
   T,
   Obj extends Record<string, any> = {}
 > = T extends `}${infer Rest}`
-  ? [{ [k in keyof Obj]: Obj[k] }, Rest]
-  : ParseKVP<Whitespace<T>> extends [infer V, infer Rest]
-  ? V extends Fail<infer Reason, infer Ctx>
-    ? [Fail<`parsing entry failed: ${Reason}`, Ctx>, T]
-    : ParseObjectEntries<Whitespace<Comma<Whitespace<Rest>>>, Obj & V>
-  : [Fail<`parsing kvp failed`, T>, T];
+  ? Success<{ [k in keyof Obj]: Obj[k] }, Rest>
+  : ParseKVP<Whitespace<T>> extends Success<infer V, infer Rest>
+    ? ParseObjectEntries<Whitespace<Comma<Whitespace<Rest>>>, Obj & V>
+    : Fail<`parsing entry failed`, T>
 
 type ParseObject<T> = T extends `{${infer XS}`
   ? ParseObjectEntries<Whitespace<XS>>
-  : [Fail<`expected {`, T>, T];
+  : Fail<`expected {`, T>
 
 type tests = {
+  simple_object: `{ "a": 5, "b": false }`;
+  empty_object: `{    }`;
+  object_with_arrays: `{ "stuff": ["1", 1, ["hello"]], "aha": false }`;
+  nested_object: `{ "point": { "x": 3, "y": 4 } }`;
+  array_with_objects: `[{ "a": 5 }, { "hello": "there" }]`;
   het_array: `[1, "nqkvi raboti", 2, true, 4, "kiril"]`;
   num_array: `[1, 2, 3, 4]`;
   bool_array: `[true, false,false,true]`;
   str_array: `["kiro","miro","gosho", "i",    "ivan"]`;
   nested: `[1, 2, ["haha"], [[[1]]]]`;
-  simple_object: `{ "a": 5, "b": false }`;
-  empty_object: `{    }`
-  object_with_arrays: `{ "stuff": ["1", 1, ["hello"]], "aha": false }`;
-  nested_object: `{ "point": { "x": 3, "y": 4 } }`;
-  array_with_objects: `[{ "a": 5 }, { "hello": "there" }]`;
   number: `32`;
   false: `false`;
   string: `"testyyy"`;
   object_array_object_array: `{ "nesting": [{ "nesting2": [1, 2, 3] }] }`;
-  empty_array: `[]`
+  empty_array: `[]`;
 };
 
 type run_tests<T extends Record<string, string>> = {
   [k in keyof T]: ParseValue<T[k]>;
 };
 
-type output = run_tests<tests>;
+type output = run_tests<tests>
