78eda91856e59453acd314569d53f0db4ebb8b9c
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-03-20 22:39:48 +0200

    clean up

--- demo.ts
+++
@@ -1,8 +0,0 @@
-import { Add, Subtr, LT } from "./data/Int"
-
-type x = [0, 1, 0, 1, 1] // 11
-type y = [0, 1, 1, 1, 0] // 14
-
-type test_add = Add<x, y>
-type test_subtr = Subtr<y, x>
-type test_less_than = LT<x, y>
